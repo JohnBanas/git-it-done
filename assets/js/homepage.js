f632e48b6de97bef007c8577d44946be69f37b4a
JPB <jbanas9124@gmail.com>
2021-03-15 01:40:42 -0500

    issue 3 resolved

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,6 +5,8 @@ var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
 
+var languageButtonsEL = document.querySelector("#language-buttons");
+
 var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -87,4 +89,36 @@ var displayRepos = function (repos, searchTerm) {
   console.log(searchTerm);
 };
 
+var getFeaturedRepos = function (language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl);
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+  }
+  console.log(language);
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEL.addEventListener("click", buttonClickHandler);
